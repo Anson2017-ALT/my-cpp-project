079b133e14ed8c2b7fe7ee97ef553b5ebf2e63e9
Anson2017-ALT <anson20170727@gmail.com>
2025-07-15 17:41:49 +0800

    Create helloworld.cpp
    hello

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // Include the input-output stream library
+
+// Entry point of the C++ program
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;  // Return 0 to indicate the program ended successfully
+}
